cfd1fd62fc1f2fe5160dcfd91d8648e44440290d
George Karagiannis <gk446@cornell.edu>
2019-11-07 17:28:19 -0500

    chore: ok

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -1,8 +1,9 @@
 from src.parser.document_parser import DocumentParser
 from src.tokenizer.tokenizer_driver import TokenizerDriver
 from src.featurizer.feature_extractor import FeatureExtractor
-from src.cluster.kmeans_cluster import KmeansCluter
+from src.cluster.kmeans_cluster import KmeansCluster
 
+from sklearn.cluster import KMeans
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -11,7 +12,7 @@ REPORTS_PATH = "data/demo_reports/"
 
 doc_parser = DocumentParser(REPORTS_PATH)
 tok_driver = TokenizerDriver()
-featurizer = FeatureExtractor()
+featurizer = FeatureExtractor(mode="word-embeddings")
 claim_sents = doc_parser.parse_documents()
 print("Number of claims: ", len(claim_sents))
 print(claim_sents[:10])
@@ -23,25 +24,24 @@ print(tokenized_sents[:5])
 
 features = featurizer.featurize_claims(tokenized_sents)
 
-print("vocabulary length: ", len(featurizer.featurizer.vocabulary_))
 print("features 1 length: ", features[0].shape)
 print("features 2 length: ", features[0].shape)
 print("Features: ")
 print(features[:5])
 
-# wcss = []
-# for i in range(22, 26):
-#     print("cluster num: ", i)
-#     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-#     kmeans.fit(features)
-#     wcss.append(kmeans.inertia_)
-# plt.plot(range(22, 26), wcss)
-# plt.title('Elbow Method')
-# plt.xlabel('Number of clusters')
-# plt.ylabel('WCSS')
-# plt.show()
+wcss = []
+for i in range(1, 50):
+    print("cluster num: ", i)
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(features)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 50), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
 
-kmeans_cluster = KmeansCluter()
+kmeans_cluster = KmeansCluster()
 clusters = kmeans_cluster.get_clusters(features)
 claims_cluster_df = kmeans_cluster.get_claims_cluster_df(tokenized_sents)
 
