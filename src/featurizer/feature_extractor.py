3ee1af588a3a3d4b848164735976a60bbfac131b
George Karagiannis <geokaragia@gmail.com>
2020-01-07 14:05:35 +0200

    chore: renaming

--- src/featurizer/feature_extractor.py
+++ src/featurizer/feature_extractor.py
@@ -1,5 +1,6 @@
 import logging
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import FeatureUnion
 
 from src.featurizer.sentence_embedding import SentenceEmbedding
 from src import helpers
@@ -9,8 +10,9 @@ class FeatureExtractor:
     def __init__(self, max_features=None, mode="word-embeddings"):
         self.nlp = helpers.get_nlp()
         if mode == "tfidf":
-            self.featurizer = TfidfVectorizer(sublinear_tf=True, min_df=1, smooth_idf=True, norm="l2", encoding="utf-8",
-                                              analyzer="word", ngram_range=(1,2))
+            self.tfidf_words = TfidfVectorizer(sublinear_tf=True, min_df=1, smooth_idf=True, norm="l2", encoding="utf-8", analyzer="word", ngram_range=(1, 2))
+            self.tfidf_chars = TfidfVectorizer(sublinear_tf=True, min_df=1, smooth_idf=True, norm="l2", encoding="utf-8", analyzer="char", ngram_range=(3, 3))
+            self.featurizer = FeatureUnion([("words", self.tfidf_words), ("chars", self.tfidf_chars)])
         elif mode == "word-embeddings":
             self.featurizer = SentenceEmbedding()
         else:
@@ -18,13 +20,13 @@ class FeatureExtractor:
         self.features = None
         self.logger = logging.getLogger(__name__)
 
-    def featurize_claims(self, tokenized_claims):
+    def featurize_train(self, tokenized_utt):
         """
         Returns anf fits features of sentences
-        :param tokenized_claims: List of strings of the tokenized claims
+        :param tokenized_utt: List of strings of the tokenized utterance
         :return: list of lists
         """
-        self.features = self.featurizer.fit_transform(tokenized_claims)
+        self.features = self.featurizer.fit_transform(tokenized_utt)
         return self.features
 
     def featurize_test(self, tokenized_test_list):
