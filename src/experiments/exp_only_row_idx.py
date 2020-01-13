60cfd2352aeb24a605621bee957feb1f9f5c4c89
George Karagiannis <geokaragia@gmail.com>
2020-01-13 12:58:22 +0200

    chore: running top3 and top5 by default and printing some advanced stats

--- src/experiments/exp_only_row_idx.py
+++ src/experiments/exp_only_row_idx.py
@@ -29,6 +29,61 @@ arg_parser.add_argument('--min_samples_per_label', type=int, help="Minumum numbe
 arg_parser.add_argument('--topn', type=int, default=5, help="Top predictions to consider")
 args = arg_parser.parse_args()
 
+def print_predictions(test_df, predictions):
+    pred_labels = [k[0] for k in predictions]
+    pred_probs = [k[1] for k in predictions]
+    test_df["pred_labels"] = pred_labels
+    test_df["pred_probs"] = pred_probs
+    test_df.to_csv("data/experiments/template_pred_01_08_2020_unscaled_probs.csv", index=False)
+
+def get_stats(predictions, y_test, topn):
+    """
+    Returns statistics from the predictions and true values
+    """
+    topn_freq = [0]*(topn + 1)
+    sum_p1_correct = 0.0
+    num_p1_correct = 0.0
+    sum_p1_incorrect = 0.0
+    num_p1_incorrect = 0.0
+    sum_p1_minus_p_correct = 0.0
+    num_p1_minus_p_correct = 0.0
+
+    num_p1_more_0_5_correct = 0.0
+    num_p1_more_0_5 = 0.0
+    for pred, label in zip(predictions, y_test):
+        pred_labels = pred[0]
+        pred_probs = pred[1]
+        correct_pred_idx = 0
+        if pred_probs[0] >= 0.5:
+            num_p1_more_0_5 += 1
+        # get sum of probabilities and number of times, the first prediction is correct
+        if pred_labels[0] == label:
+            sum_p1_correct += pred_probs[0]
+            num_p1_correct += 1
+            if pred_probs[0] >= 0.5:
+                num_p1_more_0_5_correct += 1
+        else:
+            sum_p1_incorrect += pred_probs[0]
+            num_p1_incorrect += 1
+            try:
+                correct_pred_idx = list(pred_labels).index(label)
+                p_correct = pred_probs[correct_pred_idx]
+                sum_p1_minus_p_correct += (pred_probs[0] - p_correct)
+                num_p1_minus_p_correct += 1
+            except ValueError:
+                correct_pred_idx = -1
+
+        topn_freq[correct_pred_idx] += 1
+
+    topn_freq = list(map(lambda x: x/len(y_test), topn_freq))
+    print("\n ---------- stats ----------\n")
+    print("topn_freq: {}".format(topn_freq))
+    print("average prediction probability that the first prediction is correct: {}".format(sum_p1_correct/num_p1_correct))
+    print("average prediction probability that the first prediction is incorrect: {}".format(sum_p1_incorrect/num_p1_incorrect))
+    print("average difference of the first prediction probability minus the correct probability {}".format(sum_p1_minus_p_correct/num_p1_minus_p_correct))
+    print("percentage of times p1>=0.5 and first prediction is correct: {}".format(num_p1_more_0_5_correct/num_p1_more_0_5))
+    print("\n")
+
 def join_formula_join_df(formula_df, lookup_df):
     joined_df = pd.merge(formula_df, lookup_df, on=["sent", "claim"], how="left")
     joined_df = joined_df[["sent", "claim", "formula_x", "template_formula", "row_index_y", "file_x"]]
@@ -58,7 +113,8 @@ print("num_runs = {}".format(k))
 
 tok_driver = TokenizerDriver()
 print("Running classifier for row index")
-acc = 0
+acc3 = 0
+acc5 = 0
 for run in range(0, k):
     print("Run: {}".format(run+1))
     featurizer_tf = FeatureExtractor(mode="tfidf")
@@ -75,12 +131,27 @@ for run in range(0, k):
     y_train = list(train_df["row_index"])
     y_test = list(test_df["row_index"])
     model = classifier.train(X_train, y_train)
-    _, step_acc = classifier.get_pred_and_accuracy(X_test, y_test, topn=topn)
-    print("accuracy for run {} is: {}".format(run+1, step_acc))
-    acc += step_acc
-acc = float(acc/k)
 
-print("accuracy for {} number of runs for row index {} for topn = {}".format(k, acc, topn))
+    # top3
+    print("-----top3-----\n")
+    preds3, step_acc3 = classifier.get_pred_and_accuracy(X_test, y_test, topn=3)
+    get_stats(preds3, y_test, 3)
+    print_predictions(test_df, preds3)
+    print("accuracy for run {} is: {}".format(run+1, step_acc3))
+    acc3 += step_acc3
+    # top5
+    print("-----top5-----\n")
+    preds5, step_acc5 = classifier.get_pred_and_accuracy(X_test, y_test, topn=5)
+    get_stats(preds5, y_test, 5)
+    print_predictions(test_df, preds5)
+    print("accuracy for run {} is: {}".format(run+1, step_acc5))
+    acc5 += step_acc5
+    
+acc3 = float(acc3/k)
+acc5 = float(acc5/k)
+
+print("accuracy for {} number of runs for row index {} for topn = {}".format(k, acc3, 3))
+print("accuracy for {} number of runs for row index {} for topn = {}".format(k, acc5, 5))
 
 
 
