297b09c8d81bec2b808f07ffa6473c242b03c138
George Karagiannis <gk446@cornell.edu>
2020-02-15 19:17:57 -0500

    feat: clustering pipeline ok

--- src/cluster/kmeans_cluster.py
+++ src/cluster/kmeans_cluster.py
@@ -10,27 +10,9 @@ class KmeansCluster:
     def __init__(self, num_clusters=18):
         self.kmeans = KMeans(n_clusters=num_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
         self.claim_clusters = None
-        self.df = None
         self.logger = logging.getLogger(__name__)
 
     def get_clusters(self, features):
         # cluster index for each feature
         self.claim_clusters = self.kmeans.fit_predict(features)
         return self.claim_clusters
-
-    def get_claims_cluster_df(self, claims_list):
-        """
-        For each claim, get the corresponding cluster index (from self.claim_clusters). claims_list and
-        self.claim_clusters have the same index. I.e the 3rd claim in claims_list will have as cluster
-        self.claim_clusters[4]
-        :param claims_list: List of claims
-        :return: Dataframe with two columns: claim sentence and cluster index
-        """
-        if self.claim_clusters is None:
-            self.logger.critical("Requesting Claims clusters Dataframe with None clusters")
-            return
-        print("Claims Num: {}, Clusters Num: {}".format(len(claims_list), len(self.claim_clusters)))
-        self.df = pd.DataFrame(columns=["claim", "cluster"])
-        for claim, cluster_idx in zip(claims_list, self.claim_clusters):
-            self.df = self.df.append({"claim": claim, "cluster": cluster_idx}, ignore_index=True)
-        return self.df
---
+++ src/pipeline/clustering_step.py
@@ -0,0 +1,63 @@
+from src.parser.dataset_parser import DatasetParser
+from src.tokenizer.tokenizer_driver import TokenizerDriver
+from src.featurizer.feature_extractor import FeatureExtractor
+from src.cluster.kmeans_cluster import KmeansCluster
+from src import helpers
+
+import pandas as pd
+import numpy as np
+import scipy
+
+class ClusteringStep:
+    def __init__(self, num_clusters=10):
+        self.num_clusters = num_clusters
+        self.config = helpers.load_yaml("src/config.yml")
+
+        self.tok_driver = TokenizerDriver()
+        # common featurizer objects for all tasks
+        self.featurizer_tf = None
+        self.featurizer_emb = None
+        self.clusterer = KmeansCluster(num_clusters=num_clusters)
+
+    def get_featurizers(self):
+        if self.featurizer_tf is None:
+            self.featurizer_tf = FeatureExtractor(mode="tfidf")
+
+        if self.featurizer_emb is None:
+            self.featurizer_emb = FeatureExtractor(mode="word-embeddings")
+
+        return self.featurizer_tf, self.featurizer_emb
+
+    def set_featurizers(self, f_tf, f_emb):
+        self.featurizer_tf = f_tf
+        self.featurizer_emb = f_emb
+    
+    def cluster_claims(self, df, num_clusters=10):
+        print("clustering...")
+        featurizer_tf, featurizer_emb =  self.get_featurizers()
+        sents = list(df["sent"])
+        claims = list(df["claim"])
+        X = self.get_feature_union(sents, claims, self.tok_driver, 
+                                   featurizer_emb, featurizer_tf, mode="train")
+        return self.clusterer.get_clusters(X)
+
+    @staticmethod
+    def concat_features(features_s, features_c):
+        if isinstance(features_c, scipy.sparse.csr.csr_matrix):
+            features_c = features_c.toarray()
+        if isinstance(features_s, scipy.sparse.csr.csr_matrix):
+            features_s = features_s.toarray()
+        return np.concatenate((features_s, features_c), axis=1)
+
+    def get_feature_union(self, sents, claims, tokenizer, featurizer_emb, featurizer_tf, mode="train"):
+        tokenized_sents = tokenizer.tokenize_data(sents)
+        tokenized_claims = tokenizer.tokenize_data(claims)
+        if mode == "train":
+            features_sents = featurizer_emb.featurize_train(tokenized_sents)
+            features_claims = featurizer_tf.featurize_train(tokenized_claims)
+        else:
+            features_sents = featurizer_emb.featurize_test(tokenized_sents)
+            features_claims = featurizer_tf.featurize_test(tokenized_claims)
+        features_union = self.concat_features(features_sents, features_claims)
+
+        return features_union
